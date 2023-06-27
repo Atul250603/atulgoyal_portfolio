acfd180606dd6d638bb83dcafc62867abd4ceab0
Atul250603 <93463879+Atul250603@users.noreply.github.com>
2023-06-27 20:31:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ document
           "Food Express Is a food delivery website with all the necessary functions integrated that are needed in a website of this kind.<br><mark>Technologies Used - HTML, CSS, Javascript, PHP, MYSQL</mark>";
         projectImg1 = "public/foodExpress1.png";
         projectImg2 = "public/foodExpress2.png";
-        projectLink = "https://foodsexpress.000webhostapp.com/";
+        projectLink = "http://foodexpress.42web.io/";
       } else if (parentId === "item2") {
         projectHeading = "PayBuddy";
         projectDescription =
